ff138cb84fa48aad9eeb041e8af0c2530c2830ee
Denik Syla <denik.syla@gmail.com>
2025-02-03 23:03:23 -0500

    Created Tile Context and hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { PathfindingProvider } from './context/PathfindingContext'
+import { TileProvider } from './context/TileContext';
 
 function App() {
   return (
     <PathfindingProvider>
+      <TileProvider>
       <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
         Hello world!
       </h1>
+      </TileProvider>
     </PathfindingProvider>
   );
 }
